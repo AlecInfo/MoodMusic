018442cf54606b8b7dbc8ecc5d4aa9dde97ffac3
alec <alecpiettepro@gmail.com>
2022-10-12 08:30:29 +0200

    ajout du compileur pour android

---
+++ resources/CompileAndroid.ps1
@@ -0,0 +1,17 @@
+$containerName="TestConfig"
+$pathDocker="/tmp/myapp/"
+$pathLocal="C:\Users\Admin\Desktop\TestConfig\myapp\platforms\android\app\build\outputs\apk\debug\"
+
+$Host.UI.RawUI.ForegroundColor="green"
+echo ">>>>>> Build Android Code ... `n"
+$Host.UI.RawUI.ForegroundColor="white"
+docker exec --workdir $pathDocker $containerName cordova run android
+$Host.UI.RawUI.ForegroundColor="green"
+echo ">>>>>> Install on Android Device ... `n"
+$Host.UI.RawUI.ForegroundColor="white"
+cd $pathLocal
+adb install -r app-debug.apk
+$Host.UI.RawUI.ForegroundColor="green"
+echo "`n>>>>>> END `n"
+$Host.UI.RawUI.ForegroundColor="white"
+pause
